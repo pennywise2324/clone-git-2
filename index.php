9921aa39acfb910430821c570c38489028ca4241
maufal ardian <naufal.ard.2@gmail.com>
2024-09-30 01:02:55 +0700

    tambah php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hello</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi vero sint ipsum omnis fuga dolores voluptates earum laboriosam harum tempora porro sunt atque accusamus velit, doloremque magnam et hic alias?</p>
+</body>
+</html>
